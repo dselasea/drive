3f0989be83c20998b1da8bb0c325c676d774799f
Daniel <dselasea@gmail.com>
2022-08-23 10:05:02 +0000

    Added Hero content

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import styles from './Hero.module.css/'
+import styles from './Hero.module.css'
 import {AiOutlineSearch} from 'react-icons'
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
         <form>
-            <div className="styles.text"></div>
-            <div className="styles.from"></div>
-            <div className="styles.until"></div>
-            <div className="styles.search_btn"></div>
+            <div className="styles.text">
+                <label>Where</label>
+                <input type="text" placeholder='Search Location'/>
+            </div>
+            <div className="styles.from">
+                <span className={styles.border}></span>
+                <label>From</label>
+                <input type="date" />
+            </div>
+            <div className="styles.until">
+                <span className={styles.border}></span>
+                <label>Until</label>
+                <input type="date" />
+            </div>
+            <div className="styles.search_btn">
+                {/* <button>Search</button> */}
+            </div>
         </form>
     </div>
   )
