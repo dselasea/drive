012820d5884834506f17a26acc8874220695b473
Daniel <dselasea@gmail.com>
2022-08-23 13:34:46 +0000

    Media queries for hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import styles from './Hero.module.css'
-import {AiOutlineSearch} from 'react-icons'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
         <form>
-            <div className="styles.text">
+            <div className={styles.text}>
                 <label>Where</label>
-                <input type="text" placeholder='Search Location'/>
+                <input className={styles.text_input} type="text" placeholder='Search Location'/>
             </div>
-            <div className="styles.from">
-                <span className={styles.border}></span>
+            <div className={styles.from}>
+                {/* <span className={styles.border}></span> */}
                 <label>From</label>
                 <input type="date" />
             </div>
-            <div className="styles.until">
-                <span className={styles.border}></span>
+            <div className={styles.until}>
+                {/* <span className={styles.border}></span> */}
                 <label>Until</label>
                 <input type="date" />
             </div>
-            <div className="styles.search_btn">
-                {/* <button>Search</button> */}
+            <div className={styles.search_btn}>
+                <AiOutlineSearch className={styles.icon}/>
+                <button className={styles.btn}>Search For Cars</button>
             </div>
         </form>
     </div>
