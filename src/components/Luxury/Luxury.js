6500b48d17ba32f6184feb655e4293643fa1f373
Daniel <dselasea@gmail.com>
2022-08-24 11:16:36 +0000

    Added images for the luxury component

--- src/components/Luxury/Luxury.js
+++ src/components/Luxury/Luxury.js
@@ -17,6 +17,24 @@ const Luxury = () => {
                     <h3>Rolls Royce</h3>
                 </div>
             </div>
+            <div className={styles.card}>
+                <img src="https://images.pexels.com/photos/6894429/pexels-photo-6894429.jpeg?cs=srgb&dl=pexels-sebastian-pichard-6894429.jpg&fm=jpg" alt="Maserati" />
+                <div className={styles.content}>
+                    <h3>Maserati</h3>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <img src="https://images.pexels.com/photos/4062200/pexels-photo-4062200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <div className={styles.content}>
+                    <h3>Range Rover</h3>
+                </div>
+            </div>
+            <div className={styles.card}>
+                <img src="https://images.pexels.com/photos/9805740/pexels-photo-9805740.jpeg?cs=srgb&dl=pexels-deane-bayas-9805740.jpg&fm=jpg" alt="" />
+                <div className={styles.content}>
+                    <h3>Porche</h3>
+                </div>
+            </div>
         </div>
     </div>
   )
